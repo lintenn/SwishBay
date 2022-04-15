bc373657b37d2bcd4d9261627b9431c7f727d06b
Miguel Oña Guerrero <miguelog3295@outlook.com>
2022-04-16 01:17:16 +0200

    Update productosenpuja.jsp

--- SwishBay/src/java/swishbay/dao/ProductoFacade.java
+++ SwishBay/src/java/swishbay/dao/ProductoFacade.java
@@ -11,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import swishbay.entity.Producto;
+import swishbay.entity.Puja;
 
 /**
  *
